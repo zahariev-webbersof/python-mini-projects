815a9e1f0f5580b4795c8c0e2ced4efbccf1b5dc
Mario Zahariev <webbersof@gmail.com>
2022-05-24 21:25:22 +0300

    //

---
+++ color_text.py
@@ -0,0 +1,16 @@
+from colorama import init
+from colorama import Fore, Back, Style  # Fore- font color | Back- Background
+
+init()
+
+print(Fore.GREEN, "Hello Python Dev Society")
+
+print(Back.RED, "Hello Python Dev Society")
+
+'''
+All Variaton on Colors:
+
+Fore: BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET
+Back: BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET
+Style: DIM NORMAL BRIGHT RESET_ALL
+'''
